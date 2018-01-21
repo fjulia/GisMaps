889a5cb83ccaf787a2fe1c01f5882d648bd48f45
Ferran Julia <ferran.julia@gmail.com>
2018-01-21 07:04:24 +0100

    Delete Settings.js

--- Settings.js
+++
@@ -1,9 +0,0 @@
-{
-  "api": { "key": "AIzaSyCwx0TiVW_p7-Agh-L1WXsnu3CqnQdBGg0" },
-  "kml": { "path": "dades_kml/SIG_WAYPOINTS_DARRERES.kml" },
-  "telegram": { "botToken": "145607436:AAGBUhVotQk26T9kpqm_zN8tT4BHZpmjZXM", "groupId": "groupId" },
-  "origin": {
-    "lat": 41.472821,
-    "lng": 2.035186
-  }
-}
